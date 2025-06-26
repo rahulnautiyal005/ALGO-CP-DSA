6a7a952052cc4bbbd08efef43840bbea54ec63b8
Rahul Nautiyal <rahulnautiyal123123@gmail.com>
2025-06-26 22:25:42 +0530

    Create fastIOSetup.cpp

---
+++ fastIOSetup.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* 
+Author :- Rahul_Nautiyal
+Digital Sign:- RAH_25-0612
+*/
+
+void fastIO()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+}
+
+int main()
+{
+	fastIO();
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		int a, b;
+		cin >> a >> b;
+		cout << a + b << "\n";
+	}
+	return 0;
+}
